29db7bf350ebb77bd833bfeda13f1d77da56e212
ntwcklng <ntwcklng@gmail.com>
2016-11-25 20:12:01 +0100

    script

--- getDirPrompt.js
+++ getDirPrompt.js
@@ -1,3 +1,5 @@
+const DEBUG = true
+
 const fs = require('fs')
 const inquirer = require('inquirer')
 const Github = require('github')
@@ -26,7 +28,7 @@ function createPrompt (name, message, choices, cb) {
 function readDir (dir) {
   return new Promise((resolve, reject) => {
     fs.readdir(dir, (err, files) => {
-      if (err) throw err
+      if (err) reject(err)
       resolve(files)
     })
   })
@@ -39,47 +41,55 @@ function createMarkdownImage (url, dir) {
   }
   return `![](${urlPrefix + dir + url}) \n\n`
 }
-
-readDir(rootFolder)
-.then((files) => {
-  // remove root files (images) and only list folders in the prompt
-  const rootFolders = files.filter((dir) => dir.indexOf('.') === -1)
-
-  // ask for the root folder
-  createPrompt('rootfolder', 'Select a Glossboss', rootFolders, (answer) => {
-    const selectedRootFolder = rootFolder + answer.rootfolder
-    readDir(selectedRootFolder)
-    .then((imageFolders) => {
-      // with the selected glossboss, ask for the image folder
-      createPrompt('imagefolder', 'Select an image folder', imageFolders, (answer) => {
-        const selectedImageFolder = selectedRootFolder + '/' + answer.imagefolder
-        readDir(selectedImageFolder)
-        .then((data) => {
-          return new Promise((resolve, reject) => {
-            let files = ''
-            data.map((file) => {
-              if (file.indexOf('DS_Store') > -1) return
-              files = files + createMarkdownImage(file, selectedImageFolder.replace('glossbossimages/', ''))
-            })
-            resolve(files)
-          }).then((content) => {
-            gh.gists.create({
-              'description': 'glossboss img',
-              'public': false,
-              'files': {
-                'images.md': {
-                  'content': content
-                }
-              }
-            }, (err, result) => {
-              if (err) throw err
-              console.log(result.html_url)
-              open(result.html_url)
-            })
-          })
-        })
-      })
+function generateGist (data, selectedImageFolder) {
+  return new Promise((resolve, reject) => {
+    let files = ''
+    data.map((file) => {
+      if (file.indexOf('DS_Store') > -1) return
+      files = files + createMarkdownImage(file, selectedImageFolder.replace('glossbossimages/', ''))
+    })
+    resolve(files)
+  }).then((content) => {
+    if (DEBUG) return console.log(content)
+    gh.gists.create({
+      'description': 'glossboss img',
+      'public': false,
+      'files': {
+        'images.md': {
+          'content': content
+        }
+      }
+    }, (err, result) => {
+      if (err) throw err
+      console.log(result.html_url)
+      open(result.html_url)
     })
   })
-})
+}
+function getImages (dir) {
+  // with the selected glossboss, ask for the image folder
+  readDir(dir).then((files) => {
+    // when the first file is a dir, ask for the subfolder
+    // because then i assume there are more subfolders
+    if (fs.lstatSync(dir + '/' + files[0]).isDirectory()) {
+      return createPrompt('subfolder', 'Select a Subfolder', files, (answer) => {
+        getImages(dir + '/' + answer.subfolder)
+      })
+    }
+    // no more folders... render the markdown images
+    generateGist(files, dir)
+  })
+}
+function start () {
+  readDir(rootFolder)
+  .then((files) => {
+    // remove root files (images) and only list folders in the prompt
+    const rootFolders = files.filter((dir) => dir.indexOf('.') === -1)
+    // ask for the root folder
+    createPrompt('rootfolder', 'Select a Glossboss', rootFolders, (answer) => {
+      getImages(rootFolder + answer.rootfolder)
+    })
+  })
+}
 
+start()
